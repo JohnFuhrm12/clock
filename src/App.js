ad1e274f8617f204bd440a0fca3062d7f87c8951
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-09 01:31:22 -0300

    Timer Counts Down Correctly

--- src/App.js
+++ src/App.js
@@ -1,38 +1,110 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
-  
-  const [timeLeftMinutes, setTimeLeftMinutes] = useState(25);
-  const [timeLeftSeconds, setTimeLeftSeconds] = useState(0);
+
+  const [timerLabel, setTimerLabel] = useState('Session');
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  const [timeLeftMinutes, setTimeLeftMinutes] = useState('25');
+  const [timeLeftSeconds, setTimeLeftSeconds] = useState('4');
+
+  const [formattedTime, setFormattedTime] = useState('25:00');
+
+  useEffect(() => {
+    const timer = timeLeftSeconds > 0 && timerRunning && setInterval(() => setTimeLeftSeconds(timeLeftSeconds - 1), 1000);
+    if (timerRunning && timeLeftSeconds === 0 && timeLeftMinutes >= 1) {
+      setTimeLeftSeconds('59');
+      setTimeLeftMinutes(timeLeftMinutes - 1);
+    }
+    if (timerRunning && timeLeftSeconds === '00' && timeLeftMinutes >= 1) {
+      setTimeLeftSeconds('59');
+      setTimeLeftMinutes(timeLeftMinutes - 1);
+    }
+
+    setFormattedTime(`${timeLeftMinutes}:${timeLeftSeconds}`);
+
+    return () => clearInterval(timer);
+  }, [timeLeftSeconds, timerRunning]);
+
+  function startStop() {
+    if (timerRunning) {
+      setTimerRunning(false);
+    } else {
+      setTimerRunning(true);
+    }
+  };
+
+  function reset() {
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLeftMinutes(25)
+    setTimeLeftSeconds('00')
+    setTimerRunning(false);
+  };
+
+  useEffect(() => {
+    setTimeLeftMinutes(sessionLength);
+    setFormattedTime(`${sessionLength}:${timeLeftSeconds}`);
+  }, [sessionLength])
+
+  function decrementBreak() {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
+
+  function decrementSession() {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+    }
+  };
+
+  function incrementBreak() {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  function incrementSession() {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+    }
+  };
 
   return (
-    <div className="page">
+    <>
+    <div className="wrapper">
       <div className='topRow'>
         <div className='block'>
           <h2 id="break-label">Break Length</h2>
           <div className='blockRow'>
-            <h2 id="break-decrement">-</h2>
+            <h2 onClick={decrementBreak} id="break-decrement">-</h2>
             <h2 id="break-length">{breakLength}</h2>
-            <h2 id="break-increment">+</h2>
+            <h2 onClick={incrementBreak} id="break-increment">+</h2>
           </div>
         </div>
         <div className='block'>
           <h2 id="session-label">Session Length</h2>
           <div className='blockRow'>
-            <h2 id="session-decrement">-</h2>
+            <h2 onClick={decrementSession} id="session-decrement">-</h2>
             <h2 id="session-length">{sessionLength}</h2>
-            <h2 id="session-increment">+</h2>
+            <h2 onClick={incrementSession} id="session-increment">+</h2>
           </div>
         </div>
-        <div className='timerBlock'>
-          <h2 id="timer-label">Session</h2>
-          <h2 id="time-left">{timeLeftMinutes}:{timeLeftSeconds}</h2>
-        </div>
       </div>
+        <div className='timerBlock'>
+          <h2 id="timer-label">{timerLabel}</h2>
+          <h2 id="time-left">{formattedTime}</h2>
+        </div>
+        <div className='bottomRow'>
+          <button onClick={startStop} id="start_stop">StartStop</button>
+          <button onClick={reset} id="reset">Reset</button>
+        </div>
     </div>
+    </>
   );
 }
 

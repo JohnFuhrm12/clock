025d6815c2b6cb381779dab25ece33a726af376e
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-01-08 23:08:15 -0300

    Initialized base and state components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  
+  const [timeLeftMinutes, setTimeLeftMinutes] = useState(25);
+  const [timeLeftSeconds, setTimeLeftSeconds] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Clock</h1>
+    <div className="page">
+      <div className='topRow'>
+        <div className='block'>
+          <h2 id="break-label">Break Length</h2>
+          <div className='blockRow'>
+            <h2 id="break-decrement">-</h2>
+            <h2 id="break-length">{breakLength}</h2>
+            <h2 id="break-increment">+</h2>
+          </div>
+        </div>
+        <div className='block'>
+          <h2 id="session-label">Session Length</h2>
+          <div className='blockRow'>
+            <h2 id="session-decrement">-</h2>
+            <h2 id="session-length">{sessionLength}</h2>
+            <h2 id="session-increment">+</h2>
+          </div>
+        </div>
+        <div className='timerBlock'>
+          <h2 id="timer-label">Session</h2>
+          <h2 id="time-left">{timeLeftMinutes}:{timeLeftSeconds}</h2>
+        </div>
+      </div>
     </div>
   );
 }
